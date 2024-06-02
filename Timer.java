af2e435f5a4b608aa482cb8b327d7447aa0aedca
chamer313 <167525642+chamer313@users.noreply.github.com>
2024-06-02 17:03:18 +0300

    Update Timer.java

--- Timer.java
+++ Timer.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.math.BigInteger;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
@@ -7,28 +8,28 @@ import java.util.List;
 
 public class Timer {
     public static void main(String[] args) throws IOException {
-        List<Integer> nums = Funcs.getNums("num.txt");
+        List<BigInteger> nums = Funcs.getNums("num.txt");
 
         Instant start = Instant.now();
-        int min = Funcs.findMin(nums);
+        BigInteger min = Funcs.findMin(nums);
         Instant end = Instant.now();
         long res = Duration.between(start, end).toNanos();
         System.out.println("findMin: " + res + " наносек");
 
         start = Instant.now();
-        int max = Funcs.findMax(nums);
+        BigInteger max = Funcs.findMax(nums);
         end = Instant.now();
         res = Duration.between(start, end).toNanos();
         System.out.println("findMax: " + res + " наносек");
 
         start = Instant.now();
-        int sum = Funcs.calculateSum(nums);
+        BigInteger sum = Funcs.calculateSum(nums);
         end = Instant.now();
         res = Duration.between(start, end).toNanos();
         System.out.println("calculateSum: " + res + " наносек");
 
         start = Instant.now();
-        long prod = Funcs.calculateProduct(nums);
+        BigInteger prod = Funcs.calculateProduct(nums);
         end = Instant.now();
         res = Duration.between(start, end).toNanos();
         System.out.println("calculateProduct: " + res + " наносек");
